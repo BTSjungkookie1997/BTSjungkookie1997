b9ca9c35db3a3b4071b085f1d6f6f5fabd91c890
BTSjungkookie1997 <sinhamuskan542@gmail.com>
2025-06-03 16:34:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showInfo(card) {
+  const info = card.querySelector('.plant-info');
+  info.style.display = 'block';
+}
+
+function hideInfo(card) {
+  const info = card.querySelector('.plant-info');
+  info.style.display = 'none';
+}
